be8156b391b9d80432fd2b99b8d8c0a5d22b8452
Kinga <kingaborycka@wp.pl>
2021-11-25 13:56:21 +0100

    Introduce spring integration test

--- src/main/java/pl/kiiniab/stock/App.java
+++ src/main/java/pl/kiiniab/stock/App.java
@@ -2,10 +2,23 @@ package pl.kiiniab.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.kiiniab.stock.productcatalogue.ImagesStorage;
+import pl.kiiniab.stock.productcatalogue.ProductCatalogueFacade;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public ProductCatalogueFacade productCatalogueFacade(ImagesStorage imagesStorage) {
+        return new ProductCatalogueFacade();
+    }
+
+    @Bean
+    public ImagesStorage imagesStorage() {
+        return new ImagesStorage();
     }
 }
