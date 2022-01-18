504deee9c6943e4c5f1d206145206bedede07404
Kinga <kingaborycka@wp.pl>
2022-01-18 19:36:20 +0100

    basket

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,87 +3,88 @@ const getImages = () => {
         .then(response => response.json())
         .catch(error => console.log(error));
 }
-const handleAddToBasket = (imageId) => {
-    const url = `/api/add-image/${imageId}`;
-    return fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    });
-}
 
-const refreshCurrentOffer = () => {
-    return fetch('/api/current-offer')
-        .then(r => r.json())
-        .then(offer => updateBasketComponent(offer))
-}
-
-const createHtmlElementFromString = (htmlAsString) => {
+const createHtmlElementFromString = (htmlElementAsString) => {
     let myElement = document.createElement('div');
-    myElement.innerHTML = htmlAsString.trim();
+    myElement.innerHTML = htmlElementAsString.trim();
     return myElement.firstChild;
 }
 
-const createImageComponent = (image) => {
+const createImageHtmlComponent = (image) => {
     const template = `
         <li class="image">
             <h3>${image.title}</h3>
-            <div class="image_image">
-                <img src="${image.mediaPath}" />
+            <div class="image_image-container">
+                <img src="${image.mediaPath}"/>
             </div>
             <span class="image_price">${image.price}</span>
             <button
                 class="image_add-to-basket"
                 data-image-id="${image.id}"
-            >Add to basket</button>
+            >
+                Add to basket
+            </button>
         </li>
     `;
 
     return createHtmlElementFromString(template);
 }
 
-const appendToImageList = (htmlList, htmlElements) => {
-    htmlElements.forEach(element => htmlList.appendChild(element));
+const appendImagesToList = (listEl, imagesAsHtml) => {
+    imagesAsHtml.forEach(el => listEl.appendChild(el));
 
-    return htmlElements;
+    return imagesAsHtml;
 }
 
-const updateBasketComponent = (offer) => {
+const refreshCurrentOffer = () => {
     const basketEl = document.querySelector('.basket');
-    basketEl.querySelector('.basket_items-count').innerText = `${offer.linesCount} Items`;
-    basketEl.querySelector('.basket_total').innerText = `${offer.total} PLN`;
+    fetch('/api/current-offer')
+        .then(r => r.json())
+        .then(offer => {
+            basketEl.querySelector('.basket__total').innerText = `${offer.total} PLN`;
+            basketEl.querySelector('.basket__items-count').innerText = `${offer.linesCount} Items`;
+        })
 }
 
-const initializeAddToBasketHandler = (htmlElements) => {
-    htmlElements.forEach(element => {
-        const addToBasketBtn = element.querySelector('.image_add-to-basket');
+const initializeAddToBasketHandler = (imageHtmlEl) => {
+    const addToBasketBtn = imageHtmlEl.querySelector('.image__add-to-basket');
 
-        addToBasketBtn.addEventListener('click', (event) => {
-            const imageId = event.target.getAttribute('data-image-id');
-            handleAddToBasket(imageId)
-                .then(() => refreshCurrentOffer())
-                .catch((err) => console.log('something went wrong :C'))
-        });
+    addToBasketBtn.addEventListener('click', (event) => {
+        const imageId = event.target.getAttribute('data-image-id');
+        const url = `/api/add-image/${imageId}`;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(() => refreshCurrentOffer())
+            .catch(err => console.log(err));
     })
-    return htmlElements;
+}
+
+const initializeAddToBasketHandling = (imagesAsHtmlEl) => {
+    imagesAsHtmlEl.forEach(el => initializeAddToBasketHandler(el))
+    return imagesAsHtmlEl;
+}
+
+const handleCheckout = () => {
+    console.log('lets do some checkout steps');
 }
 
 (() => {
-    console.log("It works :):)");
-
-    const imagesList = document.querySelector('.images_list');
-    const buyBtn = document.getElementById('buy');
-
-    buyBtn.addEventListener('click', () => {
-        console.log('accept offer needs to be implemented');
-    })
-
-    refreshCurrentOffer()
-        .then(() => console.log('offer should be refreshed'));
-
+    console.log("It works :)");
+    const imagesList = document.getElementById('images');
     getImages()
-        .then(imagesAsJsonObject => imagesAsJsonObject.map(createImageComponent))
-        .then(imagesAsHtmlEl => initializeAddToBasketHandler(imagesAsHtmlEl))
-        .then(imagesAsHtmlEl => appendToImageList(imagesList, imagesAsHtmlEl))
+        .then(imagesAsObjects => imagesAsObjects.map(createImageHtmlComponent))
+        .then(imagesAsHtmlEl => initializeAddToBasketHandling(imagesAsHtmlEl))
+        .then(imagesAsHtmlEl => appendImagesToList(imagesList, imagesAsHtmlEl))
+        .then(images => console.log(images))
+    ;
+
+    refreshCurrentOffer();
+
+    const checkoutBtn = document.querySelector('.basket__checkout');
+    checkoutBtn.addEventListener('click', handleCheckout);
 })();
